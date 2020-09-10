40431eff0b4f97d8b6ee0995e33783016aa0a393
Kurtis Eddy <williamkurtiseddy@gmail.com>
2020-09-10 02:48:56 -0400

    update esteps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -26,9 +26,10 @@
 /**
  * Creality CD-10 V2 options
  */
+#define EStepsmm 382.14
 
 // Is the BLTouch option installed?
-#define CR10V2_BLTOUCH
+// #define CR10V2_BLTOUCH
 
 /**
  * Configuration.h
@@ -762,7 +763,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 97 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, EStepsmm }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1116,15 +1117,15 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
-#define INVERT_Y_DIR false
-#define INVERT_Z_DIR true
+#define INVERT_X_DIR true
+#define INVERT_Y_DIR true
+#define INVERT_Z_DIR false
 
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
 #define INVERT_E0_DIR false
-#define INVERT_E1_DIR false
+#define INVERT_E1_DIR true
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
 #define INVERT_E4_DIR false
